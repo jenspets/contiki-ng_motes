2d3e4ff780249dea2c97305e0bda7dcb04406dbb
Jens-Petter <jens.petter.sandvik@gmail.com>
2021-04-22 11:34:56 +0200

    Fix latent overflow in write rate assignment

--- coffee/modifyrates_mote3.c
+++ coffee/modifyrates_mote3.c
@@ -22,9 +22,9 @@
 #define RATE3 CLOCK_SECOND * 100
 #endif
 
-#define RAND1 2934
-#define RAND2 1234
-#define RAND3 9999
+// #define RAND1 2934
+// #define RAND2 1234
+// #define RAND3 9999
 #define CANARY1 {0xe7, 0x1d, 0xe5, 0xce}
 #define CANARY2 {0xca, 0xfe, 0xba, 0xbe}
 #define CANARY3 {0xde, 0xad, 0xbe, 0xef}
@@ -39,14 +39,15 @@ PROCESS_THREAD(write_process1, ev, data){
   const char file[16] = "file0001\0";
   char msg[16];
   char canary[4] = CANARY1;
-  unsigned short seed = RAND1, tim=RATE1;
+  //unsigned short seed = RAND1, tim=RATE1;
+  unsigned long tim=RATE1;
   //cfs_offset_t offs;
   static struct etimer et1;
   
   PROCESS_BEGIN();
 
   bzero(msg, 16);
-  random_init(seed);
+  //random_init(seed);
   
   /* Test GC for file modification */
   //sprintf(file, "file0000");
@@ -76,14 +77,15 @@ PROCESS_THREAD(write_process2, ev, data){
   const char file[16] = "file0002\0";
   char msg[16];
   char canary[4] = CANARY2;
-  unsigned short seed = RAND2, tim=RATE2;
+  //unsigned short seed = RAND2, tim=RATE2;
+  unsigned long tim=RATE2;
   //cfs_offset_t offs;
   static struct etimer et2;
   
   PROCESS_BEGIN();
 
   bzero(msg, 16);
-  random_init(seed);
+  //random_init(seed);
   
   fd = cfs_open(file, CFS_WRITE);
   
@@ -111,14 +113,15 @@ PROCESS_THREAD(write_process3, ev, data){
   const char file[16] = "file0003\0";
   char msg[16];
   char canary[4] = CANARY3;
-  unsigned short seed = RAND3, tim=RATE3;
+  //unsigned short seed = RAND3, tim=RATE3;
+  unsigned long tim=RATE3;
   //cfs_offset_t offs;
   static struct etimer et3;
   
   PROCESS_BEGIN();
 
   bzero(msg, 16);
-  random_init(seed);
+  //random_init(seed);
   
   fd = cfs_open(file, CFS_WRITE);
   
